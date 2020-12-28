b6d0312d8d5d97f37840a9a65098e7dfd66e9e23
Moker6 <fomichev.roman2@yandex.ru>
2020-12-28 05:57:28 +0500

    finish

---
+++ srcipt.js
@@ -0,0 +1,13 @@
+console.log('Hello World!');
+var a = { 
+    x: 1, y: 2 
+};
+var b = a;
+b.x = 10;
+console.log(a.x);
+var с = [];
+function changeArray(arr) { 
+    arr.push(1); 
+} 
+changeArray(с);
+console.log(c);
